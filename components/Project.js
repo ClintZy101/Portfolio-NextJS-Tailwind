17facd2e47241feeb80a20fbe2ee926c1e439b41
ClintZy101 <clielizek.joy@gmail.com>
2021-07-29 16:52:12 +0900

    update

--- components/Project.js
+++ components/Project.js
@@ -9,13 +9,15 @@ const Projects = () => {
         <div className="grid  md:grid-cols-2  gap-x-3  ">
             {projects.map((p, i) => {
                 return (
-                    <div className="place-items-center my-5 container border border-none bg-gray-800 hover:bg-gray-700 rounded-lg ">
+                    <div className="place-items-center my-5 container   bg-gray-800 hover:bg-gray-700 rounded-lg border border-black">
+                        <img
+                        src={p.image} className=" w-full z-0  rounded-t-lg" />
+
                         <h1 key={i} className="text-white text-2xl  text-center py-2 font-semibold hover:text-yellow-600">{p.title}</h1>
 
                         <p className="text-white text-justify px-5 py-2 h-32 hover:text-yellow-600">{p.description}</p>
 
-                        <img
-                        src={p.image} className=" w-full z-0 " />
+                        
 
 
                         <div className="flex  text-white  hover:text-yellow-600 font-bold justify-center py-2 my-2">
